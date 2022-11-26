cdf6b2b0d0c33377dce3fbb018f9408de69946a8
floppydisk525 <mark@ijohnsen.com>
2022-11-26 11:45:10 -0800

    added tablesvisualizatioengine.
    simply copied from pablo's program :-(
    
    But..  but...  i DID use it in my SQLPractice program, so not as bad as it totally looks - haha...  ok, j/k, it's bad.

---
+++ Flashcards/TableVisualizationEngine.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleTableExt;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flashcards
+{
+    internal class TableVisualizationEngine
+    {
+        public static void ShowTable<T>(List<T> tableData, [AllowNull] string tableName) where T : class
+        {
+            if (tableName == null)
+                tableName = "";
+
+            Console.WriteLine("\n\n");
+
+            ConsoleTableBuilder
+                .From(tableData)
+                .WithTitle(tableName)
+                .ExportAndWriteLine();
+            Console.WriteLine("\n\n");
+        }
+    }
+}
